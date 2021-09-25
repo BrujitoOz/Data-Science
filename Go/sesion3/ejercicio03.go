b816c4ee96901ed39b8c8dd58a35fe089c12c1bc
BrujitoOz <ozemblem66@gmail.com>
2021-09-25 17:48:08 -0500

    25/09/21

--- Go/sesion3/ejercicio03.go
+++ Go/sesion3/ejercicio03.go
@@ -10,5 +10,6 @@ func envio(c chan int) {
 func main() {
 	c := make(chan int) // sincrona
 	go envio(c)
+	// mientras que no reciba nada, esta instruccion va a estar esperando
 	fmt.Println(<-c) // leo lo que llega a traves del canal
 }
